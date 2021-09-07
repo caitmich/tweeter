271c80e81940416b8b5d5bbd4d4f1385582e9789
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-09-07 14:59:13 -0400

    change color of counter when it reaches 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 
   $('#tweet-text').on('input', function () {
-    // console.log(this);  
     const $tweetInput = $(this).val().length;
-    console.log(140 - $tweetInput);
+    // console.log(140 - $tweetInput);
     
     const $parentSection = $(event.target).closest('section');
     let $counter = $parentSection.find('.counter');
 
     $counter.html(140 - $tweetInput);
+    console.log($counter);
+    
+    if($tweetInput > 140) {
+      $($counter).css('color', 'red');
+    } else {
+      $($counter).css('color', 'black');
+
+    }
+
   })
 
 });
